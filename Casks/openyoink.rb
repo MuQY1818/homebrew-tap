3907edbad836a1719486d1a5b33ae57793ce5b6b
weijue <weijuebu@gmail.com>
2026-08-18 14:57:51 +0800

    Update cask for repo rename and rebuilt DMG

--- Casks/openyoink.rb
+++ Casks/openyoink.rb
@@ -1,11 +1,11 @@
 cask "openyoink" do
   version "1.0"
-  sha256 "64018feb361c5ed67a89947e0974fceb6dfe3fb0549a322bbe5e92f80c32d90d"
+  sha256 "0fad5921a3bae52f019b2e2152a693b5f3d198d23c1396823ac5fb420984f96a"
 
-  url "https://github.com/MuQY1818/Open-Yoink/releases/download/v#{version}/OpenYoink-#{version}.dmg"
+  url "https://github.com/MuQY1818/OpenYoink/releases/download/v#{version}/OpenYoink-#{version}.dmg"
   name "OpenYoink"
   desc "Native macOS drag-and-drop shelf for files, text, images and links"
-  homepage "https://github.com/MuQY1818/Open-Yoink"
+  homepage "https://github.com/MuQY1818/OpenYoink"
 
   livecheck do
     url :stable
